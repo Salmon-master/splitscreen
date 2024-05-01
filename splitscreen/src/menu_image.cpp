457d1891f24031290e9cc7d6e50ce548fb911507
hugh thompson <hugh.bill@icloud.com>
2024-05-01 18:52:09 +1200

    errors

--- splitscreen/src/menu_image.cpp
+++ splitscreen/src/menu_image.cpp
@@ -6,8 +6,10 @@
 
 MenuImage::MenuImage(int x, int y, std::string name) : MenuItem(x, y) {
   // loading in image from given name
-  SetSurface(IMG_Load(("assets/menu/" + name + ".png").c_str()));
-  if (!surface_) {
+  SDL_Surface* surface_buffer =
+      IMG_Load(("assets/menu/" + name + ".png").c_str());
+  if (!surface_buffer) {
     std::cout << "image assets/menu/" + name + ".png not found" << std::endl;
   }
+  SetSurface(surface_buffer);
 }
